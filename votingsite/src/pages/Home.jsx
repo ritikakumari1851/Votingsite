4d7ee91ddcc3d9f46753cd57e736579fc6f3392d
Ritika <ritikakri3708@gmail.com>
2024-01-19 15:15:58 +0530

    Added absolute path

--- votingsite/src/pages/Home.jsx
+++ votingsite/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Slider from '../components/Slider'
-import Card from '../components/card'
+import Card from '../components/Card'
 export default function Home() {
   return (
     <div className=' flex justify-center flex-col items-center' >
