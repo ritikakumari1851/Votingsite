ae3dc4d9c07f85d3970142876b779d0c995e2cca
Ritika <ritikakri3708@gmail.com>
2024-02-11 11:52:13 +0530

    p

--- votingsite/src/pages/Auth.jsx
+++ votingsite/src/pages/Auth.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 export default function Auth() {
   const [enteredID, setEnteredID] = useState("");
