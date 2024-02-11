57b55e915cfce99f27e66537315d6b1599ca23e7
Ritika <ritikakri3708@gmail.com>
2024-02-11 10:18:28 +0530

    a

--- votingsite/src/pages/Ballot.jsx
+++ votingsite/src/pages/Ballot.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import Dashnav from "../components/Dashnav";
 import img1 from "../assets/hand-holding-ballot-paper-voting-600nw-2367185457.webp";
