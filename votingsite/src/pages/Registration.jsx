88074d16920b36dfe4333159f42c6e82f1005e6a
Ritika <ritikakri3708@gmail.com>
2024-02-29 10:31:23 +0530

    final

--- votingsite/src/pages/Registration.jsx
+++ votingsite/src/pages/Registration.jsx
@@ -232,7 +232,7 @@ export default function Registration() {
                   class="py-2 px-4 bg-blue-600 hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg"
                   type="submit"
                 >
-                  sign in
+                  sign up
                 </button>
               </div>
               <div class="flex items-center justify-between mt-4">
