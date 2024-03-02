2b317dd73afbc9411a7085e67887ed3a711fe671
Ritika <ritikakri3708@gmail.com>
2024-03-02 12:49:47 +0530

    final

--- votingsite/src/pages/Dashboard.jsx
+++ votingsite/src/pages/Dashboard.jsx
@@ -61,27 +61,23 @@ export default function Dashboard() {
               Analysis
             </p>
           </div>
-          <div className=" bg-gray-400 w-full h-[30vh] flex justify-between mx-10 shadow-xl ">
+          <div className=" w-full h-[30vh] flex justify-between mx-10 shadow-xl ">
             <div>
-              <p>
-              hello</p>
-              <img src={img1}/>
-              </div>
-              <div>
-              <p>
-              hello</p>
-              <img src={img1}/>
-              </div>
+              <p>hello</p>
+              <img src={img1} />
+            </div>
             <div>
-              <p>
-              hello</p>
-              <img src={img1}/>
-              </div>
+              <p>hello</p>
+              <img src={img1} />
+            </div>
             <div>
-              <p>
-              hello</p>
-              <img src={img1}/>
-              </div>
+              <p>hello</p>
+              <img src={img1} />
+            </div>
+            <div>
+              <p>hello</p>
+              <img src={img1} />
+            </div>
           </div>
         </div>
       </div>
